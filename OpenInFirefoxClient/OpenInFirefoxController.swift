56377e0f5da3ae5f96ac8486e8feb13ba46e4860
Bryan Munar <bmunar@mozilla.com>
2015-06-19 21:30:19 -0700

    updated both swift and obj-c files for basic callback set up

--- OpenInFirefoxClient/OpenInFirefoxController.swift
+++ OpenInFirefoxClient/OpenInFirefoxController.swift
@@ -25,43 +25,6 @@ class OpenInFirefoxController: NSObject {
         return openInFirefox(url, callbackScheme: myAppScheme, createNewTab: false)
     }
 
-    //- (BOOL)openInChrome:(NSURL *)url
-    //     withCallbackURL:(NSURL *)callbackURL
-    //        createNewTab:(BOOL)createNewTab {
-    //    NSURL *chromeSimpleURL = [NSURL URLWithString:kGoogleChromeHTTPScheme];
-    //    NSURL *chromeCallbackURL = [NSURL URLWithString:kGoogleChromeCallbackScheme];
-    //    if ([[UIApplication sharedApplication] canOpenURL:chromeCallbackURL]) {
-    //        NSString *appName =
-    //        [[NSBundle mainBundle]
-    //         objectForInfoDictionaryKey:@"CFBundleDisplayName"];
-    //
-    //        NSString *scheme = [url.scheme lowercaseString];
-    //
-    //        // Proceed only if scheme is http or https.
-    //        if ([scheme isEqualToString:@"http"] ||
-    //            [scheme isEqualToString:@"https"]) {
-    //
-    //            NSMutableString *chromeURLString = [NSMutableString string];
-    //            [chromeURLString appendFormat:
-    //             @"%@//x-callback-url/open/?x-source=%@&url=%@",
-    //             kGoogleChromeCallbackScheme,
-    //             encodeByAddingPercentEscapes(appName),
-    //             encodeByAddingPercentEscapes([url absoluteString])];
-    //            if (callbackURL) {
-    //                [chromeURLString appendFormat:@"&x-success=%@",
-    //                 encodeByAddingPercentEscapes([callbackURL absoluteString])];
-    //            }
-    //            if (createNewTab) {
-    //                [chromeURLString appendString:@"&create-new-tab"];
-    //            }
-    //
-    //            NSURL *chromeURL = [NSURL URLWithString:chromeURLString];
-    //
-    //            // Open the URL with Google Chrome.
-    //            return [[UIApplication sharedApplication] openURL:chromeURL];
-    //        }
-    //    }
-
     func openInFirefox(url: NSURL, callbackScheme: NSURL?, createNewTab: Bool) -> Bool {
         if app.canOpenURL(basicURL) {
             var scheme: NSString = url.scheme!
@@ -79,7 +42,6 @@ class OpenInFirefoxController: NSObject {
         }
         return false
     }
-
 }
 
 
