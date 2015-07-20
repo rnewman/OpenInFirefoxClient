98c4620cdb35ade3db23868e92cd2a95377fef35
Richard Newman <rnewman@twinql.com>
2015-07-20 16:57:54 -0700

    License block in AppDelegate.swift.

--- OpenInFirefoxClient/AppDelegate.swift
+++ OpenInFirefoxClient/AppDelegate.swift
@@ -1,10 +1,6 @@
-//
-//  AppDelegate.swift
-//  OpenInFirefoxClient
-//
-//  Created by Bryan Munar on 6/12/15.
-//  Copyright (c) 2015 Bryan Munar. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
 
@@ -12,7 +8,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
