5e235f56de05bcdd83464f3ca7fb74e1e900a17d
Richard Newman <rnewman@twinql.com>
2015-07-20 16:36:27 -0700

    License block in ViewController.swift.

--- OpenInFirefoxClient/ViewController.swift
+++ OpenInFirefoxClient/ViewController.swift
@@ -1,14 +1,11 @@
-//
-//  ViewController.swift
-//  OpenInFirefoxClient
-//
-//  Created by Bryan Munar on 6/12/15.
-//  Copyright (c) 2015 Bryan Munar. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
 import UIKit
 import WebKit
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
